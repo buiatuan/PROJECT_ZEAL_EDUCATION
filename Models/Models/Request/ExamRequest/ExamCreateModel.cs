16b1121908f98e6eb68a1121b02046f84ca258dd
Leaguejason <chuunbyou@gmail.com>
2023-05-13 17:12:18 +0700

    new

---
+++ Models/Models/Request/ExamRequest/ExamCreateModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Models.Models.Request.ExamRequest
+{
+	public class ExamCreateModel
+	{
+        public string? ExamCode { get; set; }
+
+        public DateTime? StartDate { get; set; }
+
+        public DateTime? EndDate { get; set; }
+
+        public int? CourseId { get; set; }
+
+        public string? Descreption { get; set; }
+    }
+}
+
---
+++ Models/Models/Response/ExamResponse/ExamBaseViewable.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Models.Entities;
+
+namespace Models.Models.Response.ExamResponse
+{
+	public class ExamBaseViewable
+	{
+        public int Id { get; set; }
+
+        public string? ExamCode { get; set; }
+
+        public DateTime? StartDate { get; set; }
+
+        public DateTime? EndDate { get; set; }
+
+        public int? CourseId { get; set; }
+
+        public string? Descreption { get; set; }
+
+        public DateTime? CreatedDate { get; set; }
+
+        public string? CreatedBy { get; set; }
+
+        public DateTime? UpdatedDate { get; set; }
+
+        public string? UpdatedBy { get; set; }
+    }
+}
+
