96a24c3494611d46800c59b1cdad70f4b0f40772
AnhTuan <devils2tuan@gmail.com>
2023-05-26 19:46:05 +0700

    add api get all feedback by courseId

--- BACKEND_ZEAL_EDUCATION/Controllers/Scholar_Client/ScholarCourseController.cs
+++ BACKEND_ZEAL_EDUCATION/Controllers/Scholar_Client/ScholarCourseController.cs
@@ -4,6 +4,7 @@ using Common;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Models.Entities;
+using Models.Models.Response;
 using Models.Models.Response.CourseResponse;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
@@ -31,6 +32,7 @@ namespace BACKEND_ZEAL_EDUCATION.Controllers.Scholar_Client
         }
 
         [HttpGet("{id:int}")]
+        [AllowAnonymous]
         public IActionResult GetDetail(int id)
         {
             var course = _dbContext.Courses.Find(id);
@@ -52,7 +54,22 @@ namespace BACKEND_ZEAL_EDUCATION.Controllers.Scholar_Client
             return Ok(result);
         }
 
-
+        [HttpGet("{id:int}")]
+        [AllowAnonymous]
+        public IActionResult GetFeedbackByCourse([FromRoute] int id) 
+        {
+            var feedbacks = _dbContext.FeedBacks.Where(m => m.CourseId == id).Select(m => 
+                new FeedbackViewable
+                {
+                    Id = m.Id,
+                    Title= m.Title,
+                    Message= m.Message,
+                    CreateBy = m.CreateBy,
+                    CreateDate= m.CreateDate,
+                    Account = m.CreateByNavigation != null ? m.CreateByNavigation.Account : null,
+                });
+            return Ok(feedbacks);
+        }
     }
 }
 
---
+++ Models/Models/Response/FeedbackViewable.cs
@@ -0,0 +1,19 @@
+﻿using Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Models.Response
+{
+    public class FeedbackViewable
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Message { get; set; }
+        public int? CreateBy { get; set; }
+        public DateTime? CreateDate { get; set; }
+        public Account? Account { get; set; }
+    }
+}
